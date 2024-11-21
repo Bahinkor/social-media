f2c9b4e71e77dc00715d9f4f91e70ba8d9bec936
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-11-21 22:19:47 +0330

    Dev error and security middlawares

--- src/app.js
+++ src/app.js
@@ -2,15 +2,26 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 // config
 dotenv.config();
 
 const app = express();
+
+// security policy middlewares
+app.use(cors());
+app.use(helmet());
+
 // parser middlewares
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({limit: "50mb", extended: true}));
 app.use(cookieParser());
+
+// logger middleware
+app.use(morgan("dev"));
 
 // static folders
 app.use(express.static(path.join(__dirname, "..", "public")));
@@ -26,6 +37,20 @@ app.set("views", path.join(__dirname, "views"));
 // routes
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+// 404 error handler
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: "this route is not found."
+    });
+});
+
+// 500 error handler
+app.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message || "Something went wrong.",
+    });
 });
 
 module.exports = app;
