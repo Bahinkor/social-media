05c01a313233ed428330aada1574d3e8bf111169
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-11-22 14:29:18 +0330

    Dev user model

---
+++ src/models/User.model.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const schema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        maxlength: 200,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        maxlength: 200,
+    },
+    biography: {
+        type: String,
+        maxlength: 500,
+    },
+    name: {
+        type: String,
+        maxlength: 200,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    profilePicture: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ["USER", "ADMIN"],
+        default: "USER",
+    },
+    private: {
+        type: Boolean,
+        default: false,
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+}, {timestamps: true});
+
+// * hashing password
+schema.pre("save", async function (next) {
+    try {
+        this.password = await bcrypt.hash(this.password, 12);
+        next();
+
+    } catch (err) {
+        console.error(`bcrypt hashing password error: ${err}`);
+        next(err);
+    }
+});
+
+const model = mongoose.models.User || mongoose.model("User", schema);
+
+module.exports = model;
