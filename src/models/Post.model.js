8961821eca05064ac65153bffafb46d1ab422c07
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-11-24 20:30:14 +0330

    Dev post model

---
+++ src/models/Post.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema({
+    media: {
+        path: {type: String, required: true},
+        filename: {type: String, required: true},
+    },
+    description: {
+        type: String,
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+});
+
+const model = mongoose.models.Post || mongoose.model("Post", schema);
+
+module.exports = model;
