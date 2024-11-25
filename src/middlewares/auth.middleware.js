e292a07da695e4e3253808e943ca2d1d66021ada
Reza Bahinkor <mohammadrezabahinkor@gmail.com>
2024-11-25 12:06:04 +0330

    Dev auth middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const userModel = require("./../models/User.model");
+
+const authMiddleware = (req, res, next) => {
+    try {
+        const accessToken = req.cookies["access-token"];
+
+        if (!accessToken) {
+            req.flash("error", "please login first.");
+            return res.redirect("/auth/login");
+        }
+
+        const payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+
+        if (!payload) {
+            req.flash("error", "please login first.");
+            return res.redirect("/auth/login");
+        }
+
+        const userID = payload.userID;
+        const user = userModel.findOne({
+            _id: userID,
+        }).lean();
+
+        if (!user) {
+            req.flash("error", "please login first.");
+            return res.redirect("/auth/login");
+        }
+
+        req.user = user;
+        next();
+
+    } catch (err) {
+        console.error(`auth middleware error: ${err}`);
+        req.flash("error", "please login first.");
+        res.redirect("/auth/login");
+    }
+};
+
+module.exports = authMiddleware;
--- src/modules/post/post.routes.js
+++ src/modules/post/post.routes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const postController = require("./post.controller");
 const validationMiddleware = require("./../../middlewares/validator.middleware")
 const {createPostValidationSchema} = require("./post.validator");
+const authMiddleware = require("./../../middlewares/auth.middleware");
 
 const postRouter = express.Router();
 
 postRouter.route("/")
-    .get(postController.showPostUploadView)
-    .post(validationMiddleware(createPostValidationSchema), postController.createPost);
+    .get(authMiddleware, postController.showPostUploadView)
+    .post(authMiddleware, validationMiddleware(createPostValidationSchema), postController.createPost);
 
 module.exports = postRouter;
